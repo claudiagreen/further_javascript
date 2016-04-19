2b8e86ee606be034e1a5755fb1c4970ad8011051
olmesm <olmesm@gmail.com>
2016-04-19 15:30:27 +0100

    ch6 compl

--- js/controllers.js
+++ js/controllers.js
@@ -1,16 +1,15 @@
-toDoApp.controller("ToDoController", function() {
+toDoApp.controller("ToDoController", ["ToDoFactory", function(ToDoFactory) {
   var list = this;
 
   list.todos = [
-    { text: "Todo1", completed: false },
-    { text: "Todo2", completed: true }
+    
   ];
 
   list.addToDo = function(toDoText) {
-    list.todos.push({ text: toDoText, completed: false });
+    list.todos.push(new ToDoFactory(toDoText));
   }
 
   list.removeToDo = function() {
     list.todos.pop();
   }
-});
+}]);
