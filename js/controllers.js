0aaa4f00ddea2bd95159a63da399a7957f55e381
Claudia Green <claudiagreen@Claudias-MacBook-Pro-2.local>
2016-04-19 16:23:58 +0100

    Chapter 7 complete. Service now working. Complete button created.

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,11 @@
-toDoApp.controller("ToDoController", ["ToDoFactory", function(ToDoFactory) {
+toDoApp.controller("ToDoController", ["ToDoService", "ToDoFactory", function(ToDoService, ToDoFactory) {
   var list = this;
 
-  list.todos = [
-    
-  ];
+  list.todos = ToDoService.getAll();
 
   list.addToDo = function(toDoText) {
     list.todos.push(new ToDoFactory(toDoText));
-  }
+  };
 
   list.removeToDo = function() {
     list.todos.pop();
